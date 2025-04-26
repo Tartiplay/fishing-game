b1db877c4b86ef47900be8a2fdb89247c16e6de6
StrigaViridis <lacrimapiya@gmail.com>
2025-04-26 12:04:37 +0200

    changed skin of sky and forests to hills

--- background.py
+++ background.py
@@ -3,8 +3,8 @@ import pyxel
 
 TILESIZE = 8
 IMG = 0
-FOREST_U = 16
-FOREST_V = 16
+HILL_U = 16
+HILL_V = 16
     
     
 
@@ -15,9 +15,9 @@ class Background:
         self.previous_player_x = 400
         
         # Forest
-        self.forest_patches_x = [400, 200, 100]
-        self.forest_w = TILESIZE * 4
-        self.forest_parallax_factor = 0.3
+        self.hill_patches_x = [0, 40, 70, 100, 140, 200, 300, 340, 400]
+        self.hill_w = TILESIZE * 4
+        self.hill_parallax_factor = 0.3
     
     
     def update(self, player):
@@ -25,24 +25,23 @@ class Background:
         delta_x = player.x - self.previous_player_x
         
         # Update layers based on parallax factor
-        # self.forest_a_x -= delta_x * self.forest_parallax_factor
-        for id, forest_x in enumerate(self.forest_patches_x):
-            forest_x -= delta_x * self.forest_parallax_factor
-            self.forest_patches_x[id] = forest_x
+        for id, hill_x in enumerate(self.hill_patches_x):
+            hill_x -= delta_x * self.hill_parallax_factor
+            self.hill_patches_x[id] = hill_x
         
         # Update the previous player x position
         self.previous_player_x = player.x
         
     
     def draw(self):
-        for forest_x in self.forest_patches_x:
+        for hill_x in self.hill_patches_x:
             pyxel.blt(
-                x = forest_x,
+                x = hill_x,
                 y = TILESIZE * 2,
                 img = IMG,
-                u = FOREST_U,
-                v = FOREST_V,
-                w = self.forest_w,
+                u = HILL_U,
+                v = HILL_V,
+                w = self.hill_w,
                 h = TILESIZE * 3,
                 colkey = 0
             )
